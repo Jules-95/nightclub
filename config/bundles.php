c7288529e8b493287e419657cebd104edb1b8dd8
Jules-95 <jlsmillet@yahoo.fr>
2026-04-20 15:28:21 +0200

    factory instal (composer require zenstruck/foundry --dev)

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\Map\UXMapBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
