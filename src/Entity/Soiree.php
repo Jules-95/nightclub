b6bdacf5520beb7d3d3e13faa5d96c469760a276
Jules-95 <jlsmillet@yahoo.fr>
2026-04-20 10:56:29 +0200

    add Entity soiree (Doctrine)

---
+++ src/Entity/Soiree.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SoireeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SoireeRepository::class)]
+class Soiree
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $datesoiree = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $datecreation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDatesoiree(): ?\DateTimeImmutable
+    {
+        return $this->datesoiree;
+    }
+
+    public function setDatesoiree(\DateTimeImmutable $datesoiree): static
+    {
+        $this->datesoiree = $datesoiree;
+
+        return $this;
+    }
+
+    public function getDatecreation(): ?\DateTimeImmutable
+    {
+        return $this->datecreation;
+    }
+
+    public function setDatecreation(\DateTimeImmutable $datecreation): static
+    {
+        $this->datecreation = $datecreation;
+
+        return $this;
+    }
+}
