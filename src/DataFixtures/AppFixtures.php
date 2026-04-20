12696b8c73a7038c4dd03938552105b8b591007e
Jules-95 <jlsmillet@yahoo.fr>
2026-04-20 15:19:15 +0200

    test modification Fixtures (+php bin/console doctrine:fixtures:load)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,21 +11,21 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $soiree1 = new Soiree();
-        $soiree1->setTitre("Soirée Mousse");
+        $soiree1->setTitre("Soirée Mousse 1");
         $soiree1->setDatesoiree(new \DateTimeImmutable('2026-06-15'));
         $soiree1->setDatecreation(new \DateTimeImmutable());
         $soiree1->setStatut("À venir");
         $manager->persist($soiree1);
 
         $soiree2 = new Soiree();
-        $soiree2->setTitre("Soirée Neon");
+        $soiree2->setTitre("Soirée Mousse 2");
         $soiree2->setDatesoiree(new \DateTimeImmutable('2026-07-20'));
         $soiree2->setDatecreation(new \DateTimeImmutable());
         $soiree2->setStatut("À venir");
         $manager->persist($soiree2);
 
         $soiree3 = new Soiree();
-        $soiree3->setTitre("Soirée Années 80");
+        $soiree3->setTitre("Soirée Mousse 3");
         $soiree3->setDatesoiree(new \DateTimeImmutable('2026-08-10'));
         $soiree3->setDatecreation(new \DateTimeImmutable());
         $soiree3->setStatut("En préparation");
