4e2e7e64cf1346ab78bdf28427f77e26d814576f
Jules-95 <jlsmillet@yahoo.fr>
2026-04-21 09:48:21 +0200

    Add Theme factory (php bin/console make:factory)

---
+++ src/Factory/ThemeFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Theme;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Theme>
+ */
+final class ThemeFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Theme::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'nom' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Theme $theme): void {})
+        ;
+    }
+}
