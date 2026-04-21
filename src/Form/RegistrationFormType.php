c0421f9a9020c728dcbc459f50dc596da4db8274
Jules-95 <jlsmillet@yahoo.fr>
2026-04-21 16:20:41 +0200

    style login and register forms with Tailwind and add validation on nom and prenom

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,19 +6,21 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class, ['required'=> true])
+            ->add('prenom', TextType::class, ['required'=> true])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
