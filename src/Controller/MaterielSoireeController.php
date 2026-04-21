8528701b55e69afdbb98f814f9398d38b25a4457
Jules-95 <jlsmillet@yahoo.fr>
2026-04-21 12:28:20 +0200

    fix: change MaterielSoiree route prefix from /materiel/soiree to /reservation to avoid conflict with Materiel routes

--- src/Controller/MaterielSoireeController.php
+++ src/Controller/MaterielSoireeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/materiel/soiree')]
+#[Route('/reservation')]
 final class MaterielSoireeController extends AbstractController
 {
     #[Route(name: 'app_materiel_soiree_index', methods: ['GET'])]
